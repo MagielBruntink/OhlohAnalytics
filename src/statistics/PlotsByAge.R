124b5da249d40085e939b9c510179c860e578e22
Magiel Bruntink <M.Bruntink@uva.nl>
2013-04-23 20:28:38 +0200

    Merged in version from amerish branch.

--- src/statistics/PlotsByAge.R
+++ src/statistics/PlotsByAge.R
@@ -7,29 +7,6 @@ analysis_dir <- "~/git/OhlohAnalytics/analysis/amerish/"
 monthlyFactsByYear <- read.csv(file=paste(analysis_dir,"allYearlyFacts.csv",sep="/"),header=TRUE,sep=",")
 projectDeath <- read.csv(file=paste(analysis_dir, "projectDeathStatus.csv",sep="/"),header=TRUE,sep=",")
 
-<<<<<<< HEAD
-### Plot of monthly growth facts aggregated by year
-
-	plotObj <- qplot(age,prod_loc_growth_factor,data=monthlyFactsByYear, group=age,geom="boxplot", xlab="Project age in years", log="y", 
-		ylab="Yearly LOC growth factor")
-	ggsave(file=paste(analysis_dir,"age-yearlygrowth.pdf",sep="/"),plot=plotObj)
-
-### Plot of monthly commits summed by year
-	plotObj <- qplot(age,sum_commits,data=monthlyFactsByYear, group=age,geom="boxplot", xlab="Project age in years", log="y",
-		ylab="Yearly sum of commits")
-	ggsave(file=paste(analysis_dir,"age-yearlycommits.pdf",sep="/"),plot=plotObj)
-
-### Plot of project survival curve
-	survivalCurve <- survfit(Surv(age,status) ~ 1, data=projectDeath)
-	pdf(file=paste(analysis_dir,"age-survival.pdf",sep="/"))
-	plot(survivalCurve, xlab="Project age in years", ylab="Project survival")
-	dev.off()
-	
-
-	
-
-
-=======
 ### Plot of project ages in 2012
 projectAgesIn2012 <- subset(monthlyFactsByYear,year==2012,select=age)
 pdf(file=paste(analysis_dir,"age-hist.pdf",sep="/"))
@@ -99,4 +76,3 @@ ggsave(file=paste(analysis_dir,"year-yearlycontributors.pdf",sep="/"),plot=plotO
   dev.off()
   
   
->>>>>>> refs/heads/amerish
