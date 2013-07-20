f81919d6ee22fdb5ad602fbd660a40b623d638f4
Magiel Bruntink <M.Bruntink@uva.nl>
2013-07-20 10:38:50 +0200

    Initial tSNE analyses.

--- src/statistics/t-SNE.py
+++ src/statistics/t-SNE.py
@@ -4,14 +4,22 @@
 import csv
 from calc_tsne import *
 
-csvFile = recfromtxt('/Users/magielbruntink/git/OhlohAnalytics/analysis/esamir/allYearlyFacts.csv', delimiter=',')
-dataMat = csvFile[1:100,5:9].astype(float)
-result = calc_tsne(dataMat,LANDMARKS=0.1)
+NO_DIMS=2
+LANDMARKS=1
+PERPLEX=5
+
+csvFile = recfromtxt('/Users/magielbruntink/git/OhlohAnalytics/analysis/tSNE-project-months-multi.csv', delimiter=',')
+dataMat = csvFile[1:,1:].astype(float)
+result = calc_tsne(dataMat,NO_DIMS=NO_DIMS,PERPLEX=PERPLEX,LANDMARKS=LANDMARKS)
 
 with open('../../output/tSNE-result.csv', 'wb') as f:
     writer = csv.writer(f)
-    writer.writerows(result[0])
+    if LANDMARKS != 1:
+        writer.writerows(result[0])
+    else:
+        writer.writerows(result)
 
-with open('../../output/tSNE-landmarks.txt', 'wb') as f:
-    for t in result[1] :
-        f.write('%s\n' % t)
+if LANDMARKS != 1:
+    with open('../../output/tSNE-landmarks.txt', 'wb') as f:
+        for t in result[1] :
+            f.write('%s\n' % t)
