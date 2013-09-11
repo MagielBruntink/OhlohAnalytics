b977b17a404fea8e2e3703b8eea8d7b11c404a6f
Magiel Bruntink <M.Bruntink@uva.nl>
2013-09-11 13:54:52 +0200

    Better naming of the data sets. Final cleaned data set without utility columns.

--- src/statistics/dataCleaning.R
+++ src/statistics/dataCleaning.R
@@ -3,29 +3,31 @@ require(plyr)
 
 analysis_dir <- "~/git/OhlohAnalytics/validation"
 
-cleanedMonthlyFacts = copy(preMonthlyValidationFacts)
-setkey(cleanedMonthlyFacts, project_name_fact, year_fact, month_fact)
+monthlyFactsDuringCleaning = copy(monthlyFactsBeforeCleaning)
+setkey(monthlyFactsDuringCleaning, project_name_fact, year_fact, month_fact)
 
 ## Remove comment_ratio_fact feature
-cleanedMonthlyFacts[,comment_ratio_fact:=NULL]
+monthlyFactsDuringCleaning[,comment_ratio_fact:=NULL]
 
 ## Remove any cases with missing values in the count features
 for(feature in c(countFeatures)) {
-  cleanedMonthlyFacts <- subset(cleanedMonthlyFacts, subset=!is.na(cleanedMonthlyFacts[[feature]]))
+  monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning, subset=!is.na(monthlyFactsDuringCleaning[[feature]]))
 }
 
 ## Remove any cases with negative values for the count features
 for(feature in c(countFeatures)) {
   featureCheckName <- paste("negative_value",feature,sep="_")
-  cleanedMonthlyFacts <- subset(cleanedMonthlyFacts, subset=is.na(cleanedMonthlyFacts[[featureCheckName]]) |
-                                                            cleanedMonthlyFacts[[featureCheckName]] == FALSE)
+  monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning, subset=is.na(monthlyFactsDuringCleaning[[featureCheckName]]) |
+                                                            monthlyFactsDuringCleaning[[featureCheckName]] == FALSE)
 }
 
 ## Remove any cases that are inconsecutive months
-cleanedMonthlyFacts <- subset(cleanedMonthlyFacts,subset=is.na(inconsecutive_month) | inconsecutive_month == FALSE)
+monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning,subset=is.na(inconsecutive_month) | inconsecutive_month == FALSE)
 
 ## Remove any cases with inconsistent values
-cleanedMonthlyFacts <- subset(cleanedMonthlyFacts,subset=is.na(inconsistent_loc) | inconsistent_loc == FALSE)
-cleanedMonthlyFacts <- subset(cleanedMonthlyFacts,subset=is.na(inconsistent_blanks) | inconsistent_blanks == FALSE)
-cleanedMonthlyFacts <- subset(cleanedMonthlyFacts,subset=is.na(inconsistent_comments) | inconsistent_comments == FALSE)
-cleanedMonthlyFacts <- subset(cleanedMonthlyFacts,subset=is.na(inconsistent_commits) | inconsistent_commits == FALSE)
+monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning,subset=is.na(inconsistent_loc) | inconsistent_loc == FALSE)
+monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning,subset=is.na(inconsistent_blanks) | inconsistent_blanks == FALSE)
+monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning,subset=is.na(inconsistent_comments) | inconsistent_comments == FALSE)
+monthlyFactsDuringCleaning <- subset(monthlyFactsDuringCleaning,subset=is.na(inconsistent_commits) | inconsistent_commits == FALSE)
+
+monthlyFactsAfterCleaning <- subset(copy(monthlyFactsDuringCleaning),select=c(keyFeatures,countFeatures))
