5acace4f4133e97dc31ed76af16b0435f3f6efa6
Magiel Bruntink <M.Bruntink@uva.nl>
2014-02-13 14:37:53 +0100

    Finished

--- src/statistics/sampleSmellyProjects.R
+++ src/statistics/sampleSmellyProjects.R
@@ -5,13 +5,21 @@ options(scipen=1000)
 analysis_dir <- "validation"
 output_dir <- "/Users/magielbruntink/Google Drive/UVA/Research/Writing/Quality of Software Evolution Data on Ohloh-SQM2014"
 
-monthlyFactsBeforeCleaningAnnotated <- data.table(read.csv(file=paste(analysis_dir,"monthlyFactsBeforeCleaningAnnotated.csv",sep="/"),header=TRUE,sep=","))
+monthlyFactsBeforeCleaningAnnotated <- data.table(read.csv(file=paste(analysis_dir,"all","monthlyFactsBeforeCleaningAnnotated.csv",sep="/"),header=TRUE,sep=","))
 
-smellyProjects <- as.vector(
-  unique(
-    monthlyFactsBeforeCleaningAnnotated[#case_has_inconsistent_values==TRUE | 
-                                             case_has_implausible_value == TRUE]$project_name_fact))
+identifySmellyProjects <- function (dt) {
+  as.vector(
+    unique(
+      dt[#case_has_inconsistent_values==TRUE | 
+        case_has_implausible_value == TRUE]$project_name_fact))
+}
 
 sampledSmellyProjects <- sample(smellyProjects,min(length(smellyProjects),800))
 
-write.csv(sampledSmellyProjects,paste(analysis_dir,"sampledSmellyProjects.csv", sep="/"))
+write.csv(sampledSmellyProjects,paste(analysis_dir, "sample", "sampledSmellyProjects.csv", sep="/"))
+
+### REFETCH PROJECTS IN RASCAL & RE_VALIDATE
+
+monthlyFactsBeforeCleaningAnnotatedSampled <- data.table(read.csv(file=paste(analysis_dir,"sample", "monthlyFactsBeforeCleaningAnnotated.csv",sep="/"),header=TRUE,sep=","))
+
+identifySmellyProjects(monthlyFactsBeforeCleaningAnnotatedSampled)
