24a27b99427a96685ebc4e63d5132adced98dcbc
Magiel Bruntink <M.Bruntink@uva.nl>
2014-02-17 21:01:35 +0100

    Regression analysis of all projects.

--- src/statistics/regressionAnalysis.R
+++ src/statistics/regressionAnalysis.R
@@ -1,9 +1,9 @@
 
-plotTrend <- function(data_df, x_str, y_str) {
-  plot(data_df, xlab=x_str, ylab=y_str)
-  lfit<-linearRegression(data_df)
-  lines(lfit$fit)
-  print(lfit$coef)
+plotTrend <- function(data_df, title_str, xlab_str, ylab_str, regressionFunction) {
+  plot(data_df, main=title_str, xlab=xlab_str, ylab=ylab_str)
+  fit_obj<-regressionFunction(data_df)
+  lines(fit_obj$fit)
+  print(summary(fit_obj))
 }
 
 linearRegression <- function(data_df) {
@@ -12,4 +12,20 @@ linearRegression <- function(data_df) {
 
 linearRegressionXY <- function(X_vec, Y_vec) {
   linearRegression(data.frame(X=X_vec,Y=Y_vec))
+}
+
+quadraticRegression <- function(data_df) {
+  qfit <- lm(Y ~ X + I(X^2), data=data_df)
+}
+
+regressionAnalysis.growth <- function (data_dt, project_str) {
+  data_for_project <- data_dt[project_str]
+  data_to_regress <- data.frame(X=as.numeric(data_for_project$age_in_months),
+                                Y=as.numeric(data_for_project$loc_fact))
+  plotTrend(data_to_regress, title_str=paste("Linear regression of growth data for", project_str, sep=" "),
+                             xlab_str="Age in months",
+                             ylab_str="LOC", linearRegression)
+  plotTrend(data_to_regress, title_str=paste("Quadratic regression of growth data for", project_str, sep=" "),
+            xlab_str="Age in months",
+            ylab_str="LOC", quadraticRegression)
 }
--- src/statistics/scratch.R
+++ src/statistics/scratch.R
@@ -1,32 +1,53 @@
 source("src/statistics/regressionAnalysis.R")
 source("src/statistics/testForNormality.R")
 
-### Absolute LOC growth
+regressionResults <- ddply(monthlyFactsAfterCleaning, .(project_name_fact), 
+      .fun = function(df) {
+        dfr = pruneOutliers(data.frame(X=df$age_in_months, Y=df$loc_fact))
+        if (nrow(na.omit(dfr)) >= 2) {
+          lin_regress_obj=linearRegression(dfr)
+          qua_regress_obj=quadraticRegression(dfr)
+          summarise(dfr,
+                    loc_fact_lin_regress_coef = lin_regress_obj$coef[2],
+                    loc_fact_lin_regress_coef_sign = sign(loc_fact_lin_regress_coef),
+                    loc_fact_lin_adj_r_squarded = summary(lin_regress_obj)$adj.r.squared,
+                    loc_fact_qua_regress_coef = qua_regress_obj$coef[3],
+                    loc_fact_qua_regress_coef_sign = sign(loc_fact_qua_regress_coef),
+                    loc_fact_qua_adj_r_squarded = summary(qua_regress_obj)$adj.r.squared)
+        }
+        else {
+          warning("Fewer than 2 non-NA cases")
+          summarise(dfr,
+                    loc_fact_lin_regress_coef = NA,
+                    loc_fact_lin_regress_coef_sign = NA,
+                    loc_fact_lin_adj_r_squarded = NA,
+                    loc_fact_qua_regress_coef = NA,
+                    loc_fact_qua_regress_coef_sign = NA,
+                    loc_fact_qua_adj_r_squarded = NA)
+        }
+      }
+)
+  
 
-monthlyFactsAfterCleaning[,abs_loc_growth_regress_coef:=
-                            linearRegression(pruneOutliers(data.frame(X=.SD$age_in_months,
-                                                                      Y=.SD$abs_loc_growth)))$coef[2],
+
+## Quadratic regression
+
+monthlyFactsAfterCleaning[,loc_fact_qua_regress_coef:=
+                            as.double(tryGettingRegressionCoefs(data.frame(X=.SD$age_in_months,
+                                                                           Y=.SD$loc_fact),
+                                                                quadraticRegression)[3]),
                           by=project_name_fact]
 
-monthlyFactsAfterCleaning[,abs_loc_growth_regress_coef_sign:=
-                            sign(abs_loc_growth_regress_coef),
+monthlyFactsAfterCleaning[,loc_fact_qua_regress_coef_sign:=
+                            sign(loc_fact_qua_regress_coef),
                           by=project_name_fact]
 
-### Commits
-
-monthlyFactsAfterCleaning[,commits_regress_coef:=
-                            linearRegression(pruneOutliers(data.frame(X=.SD$age_in_months,
-                                                                      Y=.SD$commits_fact)))$coef[2],
-                          by=project_name_fact]
-
-monthlyFactsAfterCleaning[,commits_regress_coef_sign:=
-                            sign(commits_regress_coef),
-                          by=project_name_fact]
+## Combine all results in 1 data frame
 
 regress_coefs <- subset(monthlyFactsAfterCleaning[,.SD[1],
                                                   by=project_name_fact],
                         select=c(project_name_fact,
-                                 abs_loc_growth_regress_coef,
-                                 abs_loc_growth_regress_coef_sign,
-                                 commits_regress_coef,
-                                 commits_regress_coef_sign))
+                                 loc_fact_lin_regress_coef,
+                                 loc_fact_lin_regress_coef_sign,
+                                 loc_fact_qua_regress_coef,
+                                 loc_fact_qua_regress_coef_sign))
