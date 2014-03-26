3ee18bae2067ba3b2df157b55a63369997447725
Magiel Bruntink <M.Bruntink@uva.nl>
2014-03-26 11:56:58 +0100

    Refactored into something much nicer.

--- src/dataParsing.R
+++ src/dataParsing.R
@@ -4,73 +4,75 @@ require(multicore)
 
 DataPath_str <- "data/projects"
 
-ActivityFacts_dt <- data.table(data.frame(Project_ID = character(),
-                                          Month = character(),
-                                          LOC_Added = integer(),
-                                          LOC_Deleted = integer(),
-                                          Comments_Added = integer(),
-                                          Comments_Deleted = integer(),
-                                          Blanks_Added = integer(),
-                                          Blanks_Deleted = integer(),
-                                          Commits = integer(),
-                                          Contributors = integer(),
-                                          stringsAsFactors = FALSE))
-ActivityFactsCols_vec <- as.vector(vapply(ActivityFacts_dt,class,character(1)))
-
-SizeFacts_dt <- data.table(data.frame(Project_ID = character(),
-                                      Month = character(),
-                                      LOC = integer(),
-                                      Comments = integer(),
-                                      Blanks = integer(),
-                                      Comment_Ratio = double(),
-                                      Cumulative_Commits = integer(),
-                                      Man_Months = integer(),
-                                      stringsAsFactors = FALSE))
-SizeFactsCols_vec <- as.vector(vapply(SizeFacts_dt,class,character(1)))
-
 obtainActivityFacts <- function (Project_ID_str) {
-  filePath <- paste(DataPath_str, Project_ID_str, "ActivityFacts.xml", sep="/")
-  if(file.exists(filePath)) {
-    xmlData <- xmlParse(filePath)
-    res <- data.table(xmlToDataFrame(xmlData["//activity_fact"],
-                              colClasses = tail(ActivityFactsCols_vec, -1)))
-    if(nrow(res) > 0) {
-      setnames(res,colnames(res),tail(colnames(ActivityFacts_dt),-1))
-      res[,Project_ID:=Project_ID_str][]
-      setcolorder(res, colnames(ActivityFacts_dt))
-    }
-    else {
-      ActivityFacts_dt
-    }
+  activityFacts_df <- obtainFacts(Project_ID_str, "ActivityFacts.xml", "//activity_fact")
+  if(nrow(activityFacts_df) > 0) {
+    data.frame(
+      Project_ID         = as.character(Project_ID_str),
+      Year_Month         = as.character(activityFacts_df$month),
+      LOC_Added          = as.integer(activityFacts_df$code_added),
+      LOC_Deleted        = as.integer(activityFacts_df$code_removed),
+      Comments_Added     = as.integer(activityFacts_df$comments_added),
+      Comments_Deleted   = as.integer(activityFacts_df$comments_removed),
+      Blanks_Added       = as.integer(activityFacts_df$blanks_added),
+      Blanks_Deleted     = as.integer(activityFacts_df$blanks_removed),
+      Commits            = as.integer(activityFacts_df$commits),
+      Contributors       = as.integer(activityFacts_df$contributors),
+      stringsAsFactors = FALSE)
   }
   else {
-    ActivityFacts_dt
+    data.frame()
   }
 }
 
 obtainSizeFacts <- function (Project_ID_str) {
-  filePath <- paste(DataPath_str, Project_ID_str, "SizeFacts.xml", sep="/")
-  if(file.exists(filePath)) {
-    xmlData <- xmlParse(filePath)
-    res <- data.table(xmlToDataFrame(xmlData["//size_fact"],
-                                     colClasses = tail(SizeFactsCols_vec,-1)))
-    if(nrow(res) > 0) {
-      setnames(res,colnames(res),tail(colnames(SizeFacts_dt),-1))
-      res[,Project_ID:=Project_ID_str][]
-      setcolorder(res, colnames(SizeFacts_dt))
-    }
-    else {
-      SizeFacts_dt
-    }
+  sizeFacts_df <- obtainFacts(Project_ID_str, "SizeFacts.xml", "//size_fact")
+  if(nrow(sizeFacts_df) > 0) {
+    data.frame(
+      Project_ID         = as.character(Project_ID_str),
+      Year_Month         = as.character(sizeFacts_df$month),
+      LOC                = as.integer(sizeFacts_df$code),
+      Comments           = as.integer(sizeFacts_df$comments),
+      Blanks             = as.integer(sizeFacts_df$blanks),
+      Comment_Ratio      = as.double(sizeFacts_df$comment_ratio),
+      Cumulative_Commits = as.integer(sizeFacts_df$commits),
+      Man_Months         = as.integer(sizeFacts_df$man_months),
+      stringsAsFactors = FALSE)
   }
   else {
-    SizeFacts_dt
+    data.frame()
   }
 }
 
+obtainEnlistments <- function (Project_ID_str) {
+  enlistments_df <- obtainFacts(Project_ID_str, "Enlistments.xml", "//repository")
+  if(nrow(enlistments_df) > 0) {
+      data.frame(
+        Project_ID       = as.character(Project_ID_str),
+        Type             = as.character(enlistments_df$type),
+        URL              = as.character(enlistments_df$url),
+        Logged_Date      = as.character(enlistments_df$logged_at),
+        Commits_Total    = as.integer(enlistments_df$commits),
+        stringsAsFactors = FALSE)
+  }
+  else {
+    data.frame()
+  }
+}
+
+obtainFacts <- function (Project_ID_str, XML_File_Name_str, XML_Node_str) {
+  filePath <- paste(DataPath_str, Project_ID_str, XML_File_Name_str, sep="/")
+  facts <- data.frame()
+  if(file.exists(filePath)) {
+    xmlData <- xmlParse(filePath)
+    facts <- xmlToDataFrame(xmlData[XML_Node_str])
+  }
+  return(facts)
+}
+
 dataParsing.main <- function() {
-  projects <- list.files(DataPath_str, full.names=FALSE)
-  #projects <- c("apache","firefox","mozilla","altlinux","ab")
+  #projects <- list.files(DataPath_str, full.names=FALSE)
+  projects <- c("apache","firefox","mozilla","altlinux","ab")
   allActivityFacts <- rbindlist(mclapply(projects, obtainActivityFacts))
   allSizeFacts <- rbindlist(mclapply(projects,obtainSizeFacts))
   allMergedFacts <- mergeFacts(allActivityFacts,
@@ -78,7 +80,7 @@ dataParsing.main <- function() {
 }
 
 mergeFacts <- function (facts1_dt, facts2_dt) {
-  setkey(facts1_dt, Project_ID, Month)
-  setkey(facts2_dt, Project_ID, Month)
+  setkey(facts1_dt, Project_ID, Year_Month)
+  setkey(facts2_dt, Project_ID, Year_Month)
   merge(facts1_dt,facts2_dt, all=TRUE)
 }
