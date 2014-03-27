d97fa1dea1c4daece529141f533d50ad17eab3a8
Magiel Bruntink <M.Bruntink@uva.nl>
2014-03-27 21:16:13 +0100

    Fixed differently by making character the default class when reading from XML.

--- src/dataParsing.R
+++ src/dataParsing.R
@@ -8,16 +8,16 @@ obtainActivityFacts <- function (Project_ID_str) {
   activityFacts_df <- obtainDataFromXML(Project_ID_str, "ActivityFacts.xml", "//activity_fact")
   if(nrow(activityFacts_df) > 0) {
     data.frame(
-      Project_ID         = as.character(Project_ID_str),
-      Year_Month         = as.character(activityFacts_df$month),
-      LOC_Added          = as.integer(as.character(activityFacts_df$code_added)),
-      LOC_Deleted        = as.integer(as.character(activityFacts_df$code_removed)),
-      Comments_Added     = as.integer(as.character(activityFacts_df$comments_added)),
-      Comments_Deleted   = as.integer(as.character(activityFacts_df$comments_removed)),
-      Blanks_Added       = as.integer(as.character(activityFacts_df$blanks_added)),
-      Blanks_Deleted     = as.integer(as.character(activityFacts_df$blanks_removed)),
-      Commits            = as.integer(as.character(activityFacts_df$commits)),
-      Contributors       = as.integer(as.character(activityFacts_df$contributors)),
+      Project_ID         = Project_ID_str,
+      Year_Month         = activityFacts_df$month,
+      LOC_Added          = as.integer(activityFacts_df$code_added),
+      LOC_Deleted        = as.integer(activityFacts_df$code_removed),
+      Comments_Added     = as.integer(activityFacts_df$comments_added),
+      Comments_Deleted   = as.integer(activityFacts_df$comments_removed),
+      Blanks_Added       = as.integer(activityFacts_df$blanks_added),
+      Blanks_Deleted     = as.integer(activityFacts_df$blanks_removed),
+      Commits            = as.integer(activityFacts_df$commits),
+      Contributors       = as.integer(activityFacts_df$contributors),
       stringsAsFactors = FALSE)
   }
   else {
@@ -29,14 +29,14 @@ obtainSizeFacts <- function (Project_ID_str) {
   sizeFacts_df <- obtainDataFromXML(Project_ID_str, "SizeFacts.xml", "//size_fact")
   if(nrow(sizeFacts_df) > 0) {
     data.frame(
-      Project_ID         = as.character(Project_ID_str),
-      Year_Month         = as.character(sizeFacts_df$month),
-      LOC                = as.integer(as.character(sizeFacts_df$code)),
-      Comments           = as.integer(as.character(sizeFacts_df$comments)),
-      Blanks             = as.integer(as.character(sizeFacts_df$blanks)),
-      Comment_Ratio      = as.numeric(as.character(sizeFacts_df$comment_ratio)),
-      Cumulative_Commits = as.integer(as.character(sizeFacts_df$commits)),
-      Man_Months         = as.integer(as.character(sizeFacts_df$man_months)),
+      Project_ID         = Project_ID_str,
+      Year_Month         = sizeFacts_df$month,
+      LOC                = as.integer(sizeFacts_df$code),
+      Comments           = as.integer(sizeFacts_df$comments),
+      Blanks             = as.integer(sizeFacts_df$blanks),
+      Comment_Ratio      = as.numeric(sizeFacts_df$comment_ratio),
+      Cumulative_Commits = as.integer(sizeFacts_df$commits),
+      Man_Months         = as.integer(sizeFacts_df$man_months),
       stringsAsFactors = FALSE)
   }
   else {
@@ -48,11 +48,11 @@ obtainEnlistments <- function (Project_ID_str) {
   enlistments_df <- obtainDataFromXML(Project_ID_str, "Enlistments.xml", "//repository")
   if(nrow(enlistments_df) > 0) {
       data.frame(
-        Project_ID       = as.character(Project_ID_str),
-        Type             = as.character(enlistments_df$type),
-        URL              = as.character(enlistments_df$url),
-        Logged_Date      = as.character(enlistments_df$logged_at),
-        Commits_Total    = as.integer(as.character(enlistments_df$commits)),
+        Project_ID       = Project_ID_str,
+        Type             = enlistments_df$type,
+        URL              = enlistments_df$url,
+        Logged_Date      = enlistments_df$logged_at,
+        Commits_Total    = as.integer(enlistments_df$commits),
         stringsAsFactors = FALSE)
   }
   else {
@@ -64,9 +64,9 @@ obtainMetaData <- function (Project_ID_str) {
   analysis_df <- obtainDataFromXML(Project_ID_str, "MetaData.xml", "//analysis")
   if (nrow(analysis_df) > 0) {
     data.frame (
-      Project_ID = as.character(Project_ID_str),
-      Update_Date = as.character(analysis_df$updated_at),
-      Main_Language = as.character(analysis_df$main_language_name),
+      Project_ID = Project_ID_str,
+      Update_Date = analysis_df$updated_at,
+      Main_Language = analysis_df$main_language_name,
       stringsAsFactors = FALSE)
   }
   else {
@@ -77,7 +77,7 @@ obtainMetaData <- function (Project_ID_str) {
 obtainDataFromXML <- function (Project_ID_str, XML_File_Name_str, XML_Node_str) {
   XML <- obtainXML (Project_ID_str, XML_File_Name_str)
   if(!is.null(XML)) {
-      xmlToDataFrame(XML[XML_Node_str])
+      xmlToDataFrame(XML[XML_Node_str], stringsAsFactors=FALSE)
   }
   else {
     data.frame()
